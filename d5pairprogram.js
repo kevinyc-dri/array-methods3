ba477b3a0fb99f6724fa7d5f8888c05bbea3c3e3
Kevin Choi <kevinyoungchoi@gmail.com>
2021-04-09 23:01:28 -0400

    added some notes and two ways to convert to morsecode 🚀

--- d5pairprogram.js
+++ d5pairprogram.js
@@ -26,9 +26,23 @@ const morseCode = {
   "Y": "-.--",
   "Z": "--.."
 }
+/*
+That map() is from the array method. It loops over an entire array and creates a new one with the returned values.
 
-const convertToMorse = (str) => 
-  str.toUpperCase().split("").map(el =>
-    morseCode[el] ? morseCode[el] : el).join("")
+That code is splitting a string (which creates an array) and looping over it. For each letter, it searches that letter 
+in morseCode (each letter being the key) and if it exists, it returns the object value.
+*/
+const convertToMorse = (str) =>
+  str.toUpperCase().split("").map(key => {
+    if (morseCode[key]) { // matches the key and returns the value
+      return morseCode[key]
+    } else {
+      return key
+    }
+  }).join("")
 
-console.log(convertToMorse('kevin'))
+// const convertToMorse = (str) =>
+//   str.toUpperCase().split("").map(el =>
+//     morseCode[el] ? morseCode[el] : el).join("")
+
+console.log(convertToMorse('John Doe'))
